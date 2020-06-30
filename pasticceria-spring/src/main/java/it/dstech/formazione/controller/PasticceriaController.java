11b8a5ed648d821e251e72d91f85c5a36cb072c5
utente <utente@LAPTOP-MSAI8804.homenet.telecomitalia.it>
2020-06-30 16:54:39 +0200

    primi file html

--- pasticceria-spring/src/main/java/it/dstech/formazione/controller/PasticceriaController.java
+++ pasticceria-spring/src/main/java/it/dstech/formazione/controller/PasticceriaController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import it.dstech.formazione.models.Dolce;
 import it.dstech.formazione.models.Ingrediente;
@@ -23,22 +26,36 @@ public class PasticceriaController {
 	private RicettaServiceDAO ricService;
 	@Autowired
 	private DolceServiceDAO dolceService;
+	
+	@RequestMapping("/newIngrediente")
+	public String newIngredienteForm(Model model) {
+		Ingrediente ingrediente = new Ingrediente();
+		model.addAttribute("ingrediente", ingrediente);
+		return "nuovo-ingrediente";
 
+	}
 	@GetMapping("/addIngre")
 	public String addIngrediente(Ingrediente ingrediente, Model mode) {
 		ingre.add(ingrediente);
 		mode.addAttribute("ingredienti",ingre.findAll());
-		return " ";	
+		return "admin";	
 
+	}
+	@RequestMapping("/newRicetta")
+	public String newRicettaForm(Model model) {
+		Ricetta ricetta = new Ricetta();
+		model.addAttribute("ricetta", ricetta);
+		return "nuova-ricetta";
 	}
 	
 	@GetMapping("/addRicetta")
 	public String addRicetta(Ricetta ricetta, Model mode) {
 		ricetta.setCosto();
 		ricService.add(ricetta);
-		return " ";	
+		return "admin";	
 
 	}
+	
 	
 	@PutMapping("/aggiungiIngrediente")
 	public String aggiungiIngrediente(Ricetta ricetta,Ingrediente ingrediente, Model mode) {
@@ -63,7 +80,12 @@ public class PasticceriaController {
 		return " ";	
 
 	}
-	
+	@RequestMapping("/newDolce")
+	public String newDolceForm(Model model) {
+		Dolce dolce = new Dolce();
+		model.addAttribute("dolce", dolce);
+		return "nuovo-dolce";
+	}
 	@GetMapping("/addDolce")
 	public String addDolce(Dolce dolce, Model mode) {
 		dolce.setCosto();
@@ -96,6 +118,14 @@ public class PasticceriaController {
 		return " ";	
 
 	}	
+	@GetMapping("/scelta")
+	public String scelta( @RequestAttribute String scelta) {
+		if("admin".equalsIgnoreCase(scelta)) {
+			return "admin";
+		} else {
+			return "cliente";
+		}
+	}
 	
 	
 }
